4aa225a7fb7ceef431367c3669c641668bab93a2
Georgi Tsankov <multiartdelux@abv.bg>
2024-11-16 12:20:42 +0200

    Add SearchOrder component

--- src/App.jsx
+++ src/App.jsx
@@ -5,7 +5,7 @@ import Error from "./ui/Error";
 import Menu, { loader as menuLoader } from "./Features/menu/Menu";
 import Cart from "./Features/cart/Cart";
 import CreateOrder from "./Features/order/CreateOrder";
-import Order from "./Features/order/Order";
+import Order, {loader as orderLoader} from "./Features/order/Order";
 import AppLayout from "./ui/AppLayout";
 
 const router = createBrowserRouter([
@@ -22,6 +22,7 @@ const router = createBrowserRouter([
         path: "/menu",
         element: <Menu />,
         loader: menuLoader,
+        errorElement: <Error />
       },
       {
         path: "/cart",
@@ -34,6 +35,8 @@ const router = createBrowserRouter([
       {
         path: "/order/:orderId",
         element: <Order />,
+        loader: orderLoader,
+        errorElement: <Error />,
       },
     ],
   },
--- src/ui/Header.jsx
+++ src/ui/Header.jsx
@@ -1,10 +1,13 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import SearchOrder from "../Features/order/SearchOrder";
 
 function Header() {
   return (
     <header>
-      <Link to="/">React Pizza App</Link>
+          <Link to="/">React Pizza App</Link>
+          
+          <SearchOrder />
     </header>
   );
 }
