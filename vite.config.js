91d9b44f1a772ce477707bd6ed8e7be04a878922
Georgi Tsankov <multiartdelux@abv.bg>
2024-11-19 17:03:00 +0200

    add tailwind

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,14 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-import eslint from 'vite-plugin-eslint'
+// import { defineConfig } from 'vite';
+// import react from '@vitejs/plugin-react';
+// import eslintPlugin from 'vite-plugin-eslint';
 
-// https://vitejs.dev/config/
+// export default defineConfig({
+//   plugins: [react(), eslintPlugin()],
+// });
+
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+
 export default defineConfig({
-  plugins: [react(), eslint()],
-})
+  plugins: [react()],
+});
