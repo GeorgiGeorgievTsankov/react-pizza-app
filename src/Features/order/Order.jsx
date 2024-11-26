51b78679ec6effd7a7c978bf795083d058af346e
Georgi Tsankov <multiartdelux@abv.bg>
2024-11-26 09:13:09 +0200

    Update CreateOrder and Order components; fix userSlice logic

--- src/Features/order/Order.jsx
+++ src/Features/order/Order.jsx
@@ -8,6 +8,7 @@ import {
 } from "../../utils/helpers";
 import { getOrder } from "../../services/apiRestaurant";
 import { useLoaderData } from "react-router-dom";
+import OrderItem from "./OrderItem";
 
 
 
@@ -57,7 +58,7 @@ function Order() {
 
       <ul className="dive-stone-200 divide-y border-b border-t">
         {cart.map((item) => (
-          <OrderItem item={item} key={item.id} />
+          <OrderItem item={item} key={item.pizzaId} />
         ))}
       </ul>
 
